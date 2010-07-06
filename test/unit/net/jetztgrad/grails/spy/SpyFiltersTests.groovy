26d55c8cf5075a504e5364dc957c3b442663ef2d
Wolfgang Schell <github@jetztgrad.net>
2010-07-06 23:18:01 +0200

    upgraded to Grails 1.3.2

---
+++ test/unit/net/jetztgrad/grails/spy/SpyFiltersTests.groovy
@@ -0,0 +1,17 @@
+package net.jetztgrad.grails.spy
+
+import grails.test.*
+
+class SpyFiltersTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
